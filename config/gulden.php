d5dfb48728d2fc09aac88aa3cb9e3b55e61b3d2a
Bram van Berkel <b.vanberekel@solvari.com>
2020-06-02 22:07:17 +0200

    add gulden config varables

--- config/gulden.php
+++ config/gulden.php
@@ -1,8 +1,27 @@
 <?php
 
 return [
-    'rpc_user' => env('GULDEN_RPC_USER'),
-    'rpc_password' => env('GULDEN_RPC_PASSWORD'),
-    'rpc_host' => env('GULDEN_RPC_HOST'),
-    'rpc_port' => env('GULDEN_RPC_PORT')
+    /*
+    |--------------------------------------------------------------------------
+    | Gulden RPC settings
+    |--------------------------------------------------------------------------
+    |
+    | These RPC settings are automatically configured for the Gulden service
+    | and used to connect via RPC.
+    |
+    */
+    'rpc_user' => env('GULDEN_RPC_USER', 'gulden'),
+    'rpc_password' => env('GULDEN_RPC_PASSWORD', 'secret'),
+    'rpc_host' => env('GULDEN_RPC_HOST', ''),
+    'rpc_port' => env('GULDEN_RPC_PORT', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blocknotify
+    |--------------------------------------------------------------------------
+    |
+    | The command to execute when a new block is found
+    |
+    */
+    'blocknotify' => env('GULDEN_BLOCKNOTIFY')
 ];
