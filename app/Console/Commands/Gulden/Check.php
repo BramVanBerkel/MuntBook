15f1654ee4074693f8003219de23dfc7d4321ebb
Bram van Berkel <b.vanberekel@solvari.com>
2020-06-06 21:39:57 +0200

    add check if there are new blocks

--- app/Console/Commands/Gulden/Check.php
+++ app/Console/Commands/Gulden/Check.php
@@ -14,6 +14,7 @@ use Psr\SimpleCache\InvalidArgumentException;
 class Check extends Command
 {
     use WithoutOverlapping;
+    protected $verbosity
 
     /**
      * The name and signature of the console command.
@@ -50,6 +51,10 @@ class Check extends Command
     {
         $guldenBlockCount = $guldenService->getBlockCount();
         $dbBlockCount = Block::count();
+
+        if($dbBlockCount === $guldenBlockCount) {
+            return;
+        }
 
         foreach(range($dbBlockCount, $guldenBlockCount) as $height) {
             if(!Cache::has("syncblock-{$height}")) {
