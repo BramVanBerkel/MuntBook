7014262a514be8f80216abd77a1bc83115ad762a
Bram van Berkel <b.vanberekel@solvari.com>
2020-08-24 20:23:16 +0200

    remove use

--- app/Jobs/SyncBlock.php
+++ app/Jobs/SyncBlock.php
@@ -12,7 +12,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class SyncBlock implements ShouldQueue
