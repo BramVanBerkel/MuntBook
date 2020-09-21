220802c017bfa036ed5494da3886685c4a513a45
Bram van Berkel <b.vanberekel@solvari.com>
2020-09-21 22:29:01 +0200

    add next & previous block links

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -11,6 +11,8 @@ class BlockController extends Controller
     public function index(Block $block)
     {
         return view('layouts.pages.block')->with([
+            'previousBlock' => (Block::find($block->height - 1)) ? $block->height - 1 : null,
+            'nextBlock' => (Block::find($block->height + 1)) ? $block->height + 1 : null,
             'block' => $block
         ]);
     }
--- resources/views/layouts/pages/block.blade.php
+++ resources/views/layouts/pages/block.blade.php
@@ -7,37 +7,37 @@
                 <h2 class="section-title">Details for block #{{ $block->height }}</h2>
             </div>
         </div>
-{{--        <div class="offset-lg-3 col-lg-6">--}}
-{{--            <div class="center-text">--}}
-{{--                <p>Fusce placerat pretium mauris, vel sollicitudin elit lacinia vitae. Quisque sit amet nisi erat.</p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+        {{--        <div class="offset-lg-3 col-lg-6">--}}
+        {{--            <div class="center-text">--}}
+        {{--                <p>Fusce placerat pretium mauris, vel sollicitudin elit lacinia vitae. Quisque sit amet nisi erat.</p>--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
     </div>
     <div class="row m-bottom-70">
         <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="table-responsive">
                 <table class="table table-striped table-latests table-detail">
                     <tbody>
-                        <tr>
-                            <td><strong>Timestamp</strong></td>
-                            <td>{{ $block->created_at }}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Total value out</strong></td>
-                            <td><span class="gulden-icon"></span> {{ $block->total_value_out }}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>No. of transactions</strong></td>
-                            <td>{{ $block->transactions->count() }}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Version</strong></td>
-                            <td>{{ $block->version }}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Merkle root</strong></td>
-                            <td>{{ $block->merkleroot }}</td>
-                        </tr>
+                    <tr>
+                        <td><strong>Timestamp</strong></td>
+                        <td>{{ $block->created_at }}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Total value out</strong></td>
+                        <td><span class="gulden-icon"></span> {{ $block->total_value_out }}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>No. of transactions</strong></td>
+                        <td>{{ $block->transactions->count() }}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Version</strong></td>
+                        <td>{{ $block->version }}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Merkle root</strong></td>
+                        <td>{{ $block->merkleroot }}</td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
@@ -55,10 +55,10 @@
             <div class="table-responsive">
                 <table class="table table-striped table-latests table-detail">
                     <thead>
-                        <tr>
-                            <th>Transaction id</th>
-                            <th>Transaction ammount</th>
-                        </tr>
+                    <tr>
+                        <th>Transaction id</th>
+                        <th>Transaction ammount</th>
+                    </tr>
                     </thead>
                     <tbody>
                     @foreach($block->transactions as $transaction)
@@ -71,10 +71,28 @@
                             <td><span class="gulden-icon"></span> {{ $transaction->total_value_out }}</td>
                         </tr>
                     @endforeach
-
                     </tbody>
                 </table>
             </div>
         </div>
+    </div>
+    <div class="row">
+        @if($previousBlock)
+            <span class="mr-auto">
+                <a href="{{ route('block', ['block' => $previousBlock]) }}">
+                    <i class="fa fa-angle-double-left"></i>
+                    Previous block
+                </a>
+            </span>
+        @endif
+
+        @if($nextBlock)
+            <span class="ml-auto">
+                <a href="{{ route('block', ['block' => $nextBlock]) }}">
+                    Next block
+                    <i class="fa fa-angle-double-right"></i>
+                </a>
+            </span>
+        @endif
     </div>
 @endsection
