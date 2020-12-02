3485dd7e9a3aefa7d3aca3179e5343e756e333a9
Bram van Berkel <b.vanberekel@solvari.com>
2020-12-02 09:26:38 +0100

    remove commented scripts

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,16 +1,1 @@
-<!-- jQuery -->
 <script src="{{ asset('js/app.js') }}"></script>
-
-{{--<!-- Bootstrap -->--}}
-{{--<script src="assets/js/popper.js"></script>--}}
-{{--<script src="assets/js/bootstrap.min.js"></script>--}}
-
-{{--<!-- Plugins -->--}}
-{{--<script src="assets/js/particles.min.js"></script>--}}
-{{--<script src="assets/js/scrollreveal.min.js"></script>--}}
-{{--<script src="assets/js/jquery.downCount.js"></script>--}}
-{{--<script src="assets/js/parallax.min.js"></script>--}}
-
-{{--<!-- Global Init -->--}}
-{{--<script src="assets/js/particle-blue.js"></script>--}}
-{{--<script src="assets/js/custom.js"></script>--}}
