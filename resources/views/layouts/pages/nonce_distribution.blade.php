62244bd86ffd05f221c0fbdd1bf069e1bcd49473
Bram van Berkel <b.vanberekel@solvari.com>
2020-12-05 12:41:25 +0100

    fix stepSize in chart

--- resources/views/layouts/pages/nonce_distribution.blade.php
+++ resources/views/layouts/pages/nonce_distribution.blade.php
@@ -27,16 +27,6 @@
         let nonceDistributionCanvas = document.getElementById('nonceDistribution').getContext('2d');
         let nonceDistributionChart = new Chart(nonceDistributionCanvas, {
             type: 'scatter',
-            data: {},
-            options: {
-                scales: {
-                    xAxes: [{
-                        ticks: {
-                            stepSize: 1
-                        }
-                    }]
-                }
-            }
         });
 
         ajax_chart(nonceDistributionChart, url);
@@ -44,8 +34,11 @@
         // function to update our chart
         function ajax_chart(chart, url) {
             $.getJSON(url).done(function(response) {
+                let min = response.datasets[0].data[response.datasets[0].data.length - 1].x;
                 chart.data = response;
-                chart.update(); // finally update our chart
+                chart.options.scales.xAxes[0].ticks.min = min
+                chart.options.scales.xAxes[0].ticks.stepSize = 100;
+                chart.update();
             });
         }
     </script>
