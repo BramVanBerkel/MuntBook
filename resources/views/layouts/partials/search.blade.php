fb5dd96a244d186b921eaf199486ee36dc0a1be9
Bram van Berkel <bramvanberkel.prive@gmail.com>
2020-12-23 19:39:48 +0100

    add search help

--- resources/views/layouts/partials/search.blade.php
+++ resources/views/layouts/partials/search.blade.php
@@ -6,7 +6,9 @@
                     <div class="input">
                         <form method="POST" action="{{ route('search') }}">
                             @csrf
-                            <input aria-label="Search" type="text" name="query" placeholder="block, hash, transaction, etc...">
+                            <input aria-label="Search" type="text" name="query" placeholder="Search" id="query">
+                            <small id="queryHelp" class="form-text text-muted">Search for blocks, transactions or
+                                addresses</small>
                             <button aria-label="Search submit" type="submit"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
