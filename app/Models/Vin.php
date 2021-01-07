1975dab10ca06b701ac78bdc850bb21431a056ee
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-01-07 23:10:41 +0100

    Revert "remove unused vin columns"
    This reverts commit b5c9fc43be07b58ba25db474eb54f1d11855cde6.

--- app/Models/Vin.php
+++ app/Models/Vin.php
@@ -14,12 +14,25 @@ class Vin extends Model
     protected $table = 'vins';
 
     protected $fillable = [
+        'prevout_type',
+        'coinbase',
+        'tx_height',
+        'tx_index',
+        'scriptSig_asm',
+        'scriptSig_hex',
+        'vout',
+        'rbf',
         'transaction_id',
         'vout_id',
     ];
 
     const PREVOUT_TYPE_INDEX = 'index';
     const PREVOUT_TYPE_HASH = 'hash';
+
+    const PREVOUT_TYPES = [
+        self::PREVOUT_TYPE_INDEX,
+        self::PREVOUT_TYPE_HASH,
+    ];
 
     public function transaction()
     {
