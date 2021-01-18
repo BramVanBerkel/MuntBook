6027eb17b13384ac3074d29d100e3d5aee5a5688
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-01-18 22:41:24 +0100

    remove commented code

--- resources/views/layouts/pages/difficulty.blade.php
+++ resources/views/layouts/pages/difficulty.blade.php
@@ -43,18 +43,13 @@
                 }
             }
         });
-        console.log()
 
         ajax_chart(difficultyChart, url);
 
         // function to update our chart
         function ajax_chart(chart, url) {
             $.getJSON(url).done(function(response) {
-                console.log(response);
-                // let min = response.datasets[0].data[response.datasets[0].data.length - 1].x;
                 chart.data = response;
-                // chart.options.scales.xAxes[0].ticks.min = min
-                // chart.options.scales.xAxes[0].ticks.stepSize = 100;
                 chart.update();
             });
         }
