9da26e61a07b4336291b3bd8eacc5594f4a18760
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-01-19 17:15:21 +0100

    refactor javascript for updating the graph

--- resources/views/layouts/pages/difficulty.blade.php
+++ resources/views/layouts/pages/difficulty.blade.php
@@ -24,35 +24,35 @@
         <div class="col-lg-6">
             <div class="btn-group btn-group-toggle" id="timeframes" data-toggle="buttons">
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="timeframe" value="30" name="options" autocomplete="off"> 30 days
+                    <input type="radio" class="chart-setting" value="30" name="options" autocomplete="off"> 30 days
                 </label>
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="timeframe" value="60" name="options" autocomplete="off"> 60 days
+                    <input type="radio" class="chart-setting" value="60" name="options" autocomplete="off"> 60 days
                 </label>
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="timeframe" value="180" name="options" autocomplete="off" checked> 180 days
+                    <input type="radio" class="chart-setting" value="180" name="options" autocomplete="off" checked> 180 days
                 </label>
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="timeframe" value="365" name="options" autocomplete="off"> 1 year
+                    <input type="radio" class="chart-setting" value="365" name="options" autocomplete="off"> 1 year
                 </label>
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="timeframe" value="1095" name="options" autocomplete="off"> 3 years
+                    <input type="radio" class="chart-setting" value="1095" name="options" autocomplete="off"> 3 years
                 </label>
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="timeframe" value="-1" name="options" autocomplete="off"> All Time
+                    <input type="radio" class="chart-setting" value="-1" name="options" autocomplete="off"> All Time
                 </label>
             </div>
         </div>
         <div class="col-lg-6">
             <div class="btn-group btn-group-toggle float-right" id="averages" data-toggle="buttons">
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="average" value="1" name="options" autocomplete="off"> Raw Values
+                    <input type="radio" class="chart-setting" value="1" name="options" autocomplete="off"> Raw Values
                 </label>
                 <label class="btn btn-outline-primary active">
-                    <input type="radio" class="average" value="7" name="options" autocomplete="off" checked> 7 day average
+                    <input type="radio" class="chart-setting" value="7" name="options" autocomplete="off" checked> 7 day average
                 </label>
                 <label class="btn btn-outline-primary">
-                    <input type="radio" class="average" value="30" name="options" autocomplete="off"> 30 day average
+                    <input type="radio" class="chart-setting" value="30" name="options" autocomplete="off"> 30 day average
                 </label>
             </div>
         </div>
@@ -90,17 +90,9 @@
             });
         }
 
-        $('#timeframes .timeframe').click(function(event){
-            let timeframe = $(event.target).attr('value');
-            let average = $('#averages .active input').attr('value');
-            console.log('timeframe: ' + timeframe + ', average: ' + average);
-            ajax_chart(timeframe, average);
-        });
-
-        $('#averages .average').click(function(event){
+        $('.chart-setting').change(function(event) {
             let timeframe = $('#timeframes .active input').attr('value');
-            let average = $(event.target).attr('value');
-            console.log('timeframe: ' + timeframe + ', average: ' + average);
+            let average = $('#averages .active input').attr('value');
             ajax_chart(timeframe, average);
         });
 
