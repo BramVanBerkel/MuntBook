a91eb6ea3e048ecf323b1c6363db5e5fc3a5774a
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-03-19 11:51:50 +0100

    add bittrex prices seeder

---
+++ database/seeds/BittrexPricesSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BittrexPricesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //unzip
+        $zipPath = realpath('database/seeds/bittrex.csv.zip');
+        $zipArchive = new \ZipArchive();
+
+        \Log::channel('stderr')->info('Openging bittrex.csv.zip...');
+        $opened = $zipArchive->open($zipPath);
+        if(!$opened) {
+            \Log::channel('stderr')->info('Failed opening bittrex.csv.zip!');
+            die();
+        }
+
+        \Log::channel('stderr')->info('Unzipping bittrex.csv.zip...');
+        $extracted = $zipArchive->extractTo('database/seeds');
+        if(!$extracted) {
+            \Log::channel('stderr')->info('Failed extracting bittrex.csv.zip!');
+            die();
+        }
+
+        $path = realpath('database/seeds/bittrex.csv');
+
+        //import
+        \Log::channel('stderr')->info('Importing bittrex.csv.zip...');
+        DB::statement(<<<SQL
+            COPY prices.bittrex(timestamp, open, high, low, close, volume, quote_volume)
+            FROM '{$path}'
+            DELIMITER ','
+            CSV HEADER;
+        SQL);
+
+        //cleanup
+        \Log::channel('stderr')->info('Removing bittrex.csv.zip...');
+        unlink($path);
+    }
+}
