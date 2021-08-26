d1dd4569bdeae3a571d3512a1fb5a0cd6eef3b6b
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-08-26 23:02:18 +0200

    add WitnessAddressPartsSeeder

---
+++ database/seeds/WitnessAddressPartsSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Address\Address;
+use App\Models\WitnessAddressPart;
+use Illuminate\Database\Seeder;
+
+class WitnessAddressPartsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $addresses = json_decode(file_get_contents("zip://database/seeds/addresses.json.zip#addresses.json"), true);
+
+        foreach ($addresses as $address => $parts) {
+            $address = Address::firstWhere('address', '=', $address);
+
+            foreach ($parts as $part) {
+                WitnessAddressPart::create([
+                    'address_id' => $address->id,
+                    'type' => $part['type'],
+                    'age' => $part['age'],
+                    'amount' => $part['amount'],
+                    'raw_weight' => $part['raw_weight'],
+                    'adjusted_weight' => $part['adjusted_weight'],
+                    'adjusted_weight_final' => $part['adjusted_weight_final'],
+                    'expected_witness_period' => $part['expected_witness_period'],
+                    'estimated_witness_period' => $part['estimated_witness_period'],
+                    'last_active_block' => $part['last_active_block'],
+                    'lock_from_block' => $part['lock_from_block'],
+                    'lock_until_block' => $part['lock_until_block'],
+                    'lock_period' => $part['lock_period'],
+                    'lock_period_expired' => $part['lock_period_expired'],
+                    'eligible_to_witness' => $part['eligible_to_witness'],
+                    'expired_from_inactivity' => $part['expired_from_inactivity'],
+                    'fail_count' => $part['fail_count'],
+                    'action_nonce' => $part['action_nonce'],
+                ]);
+            }
+        }
+    }
+}
