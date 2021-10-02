a440bfe36c0c2467ffa03ee01ae95e06ba29198f
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-10-02 19:46:13 +0200

    fix date format

--- resources/views/components/date.blade.php
+++ resources/views/components/date.blade.php
@@ -1,4 +1,4 @@
 <span x-data='{ date: @json($date) }'
-    x-text="new Date(date + ' UTC').toLocaleString('nl-NL')">
+    x-text="new Date(date + ' UTC').toLocaleString('nl-NL', {month: '2-digit', day: '2-digit', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'})">
     <noscript>{{ $date }}</noscript>
 </span>
