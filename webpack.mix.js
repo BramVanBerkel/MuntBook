b00f862c982e01b1cc5cd5cf0d0a25faa4b562f6
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-10-09 17:21:07 +0200

    add mixing of images back

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,26 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]);
+
+if (process.env.MIX_TESTNET === 'true') {
+    console.log('Building for testnet...');
+
+    mix.copy('resources/images/icon/testnet/*', 'public/images/icon')
+        .copy('resources/images/logos/testnet/*', 'public/images/logos')
+        .copy('resources/images/splash/testnet/*', 'public/images/splash')
+        .copy('resources/webmanifest/testnet.webmanifest', 'public/site.webmanifest');
+} else {
+    console.log('Building for mainnet...');
+
+    mix.copy('resources/images/icon/mainnet/*', 'public/images/icon')
+        .copy('resources/images/logos/mainnet/*', 'public/images/logos')
+        .copy('resources/images/splash/mainnet/*', 'public/images/splash')
+        .copy('resources/webmanifest/mainnet.webmanifest', 'public/site.webmanifest');
+}
+mix.copy('resources/images/favicon.ico', 'public/images')
