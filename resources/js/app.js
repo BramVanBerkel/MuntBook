05336c886487ad5375bc557ed9ab389d880e7012
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-10-20 10:07:24 +0200

    remove unused searchApp

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,3 @@
 require('./bootstrap');
 
 require('alpinejs');
-
-window.searchApp = () => {
-    return {
-        query: null,
-
-        search() {
-            console.log(this.query)
-        }
-    }
-}
