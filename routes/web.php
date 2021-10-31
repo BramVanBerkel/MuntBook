389d7d90b24a2b80a34c3fbc79c9e0cbca312089
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-10-31 20:39:31 +0100

    remove dashboard route

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,4 @@ Route::get('/transaction/{txid}', [TransactionController::class, 'index'])->name
 
 Route::get('/address/{address}', [AddressController::class, 'index'])->name('address');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
