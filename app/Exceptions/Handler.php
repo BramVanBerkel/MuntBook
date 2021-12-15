b169798d66da040dd6ca459a54990fbe30c80623
Bram van Berkel <bramvanberkel.prive@gmail.com>
2021-12-15 09:39:06 +0100

    don't report MutexRuntimeException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminated\Console\MutexRuntimeException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,7 +14,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        MutexRuntimeException::class,
     ];
 
     /**
