c566cb0c771a6c4f083e728b6ef4a45bad7f4401
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-01-09 21:53:54 +0100

    move to tailwind v3

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -3,6 +3,6 @@
     'href' => '',
 ])
 
-<a href="{{ $href }}" {{ $attributes->class(['text-blue-600 hover:text-blue-800 visited:text-purple-600' => $styled]) }}>
+<a href="{{ $href }}" {{ $attributes->class(['text-blue-600 hover:text-blue-800' => $styled]) }}>
     {{ $slot }}
 </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
+    content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -15,11 +15,7 @@ module.exports = {
         },
     },
 
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
-    },
-
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 };
