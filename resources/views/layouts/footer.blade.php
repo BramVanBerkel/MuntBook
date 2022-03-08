14b098ee8df4b04a83d530c464a98853a7af5c65
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-03-08 10:58:39 +0100

    remove tailwind css notice

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,3 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <footer class="bg-white">
     <div class="max-w-7xl mx-auto py-7 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div class="mt-8 md:mt-0 md:order-1">
