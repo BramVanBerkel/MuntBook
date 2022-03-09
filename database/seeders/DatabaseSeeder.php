9845ea7ddc5bd65d93b5899c572e2ce53c16ade3
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-03-09 09:35:13 +0100

    add seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->call([
+            UsersSeeder::class,
+            WitnessAddressPartsSeeder::class,
+        ]);
     }
 }
