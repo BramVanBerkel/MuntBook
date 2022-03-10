133dbc650ce7cf69307724aadc3525186f384223
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-03-10 18:38:14 +0100

    show current page in dropdown nav

--- resources/views/components/navigation/nav-group-item.blade.php
+++ resources/views/components/navigation/nav-group-item.blade.php
@@ -2,7 +2,24 @@
     'route',
 ])
 
-<a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+<a @class([
+    'block',
+    'px-4',
+    'py-2',
+    'mt-2',
+    'text-sm',
+    'font-semibold',
+    'bg-transparent',
+    'rounded-lg',
+    'md:mt-0',
+    'hover:text-gray-900',
+    'focus:text-gray-900',
+    'hover:bg-gray-200',
+    'focus:bg-gray-200',
+    'focus:outline-none',
+    'focus:shadow-outline',
+    'bg-gray-300' => request()->is($route)
+])
    href="{{ route($route) }}">
     {{ $slot }}
 </a>
