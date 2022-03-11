60a07709f8358ee8e0f82df5f87f8c7a9fce6a64
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-03-11 10:07:29 +0100

    add border around table

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,8 +1,8 @@
-<div class="pt-5 flex flex-col">
+<div class="py-5 flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden sm:rounded-lg">
-                <table class="min-w-full divide-y divide-gray-200">
+            <div class="overflow-hidden ring-1 ring-black ring-opacity-5  sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200 ring-1 ring-black ring-opacity-5">
                     {{ $slot }}
                 </table>
             </div>
