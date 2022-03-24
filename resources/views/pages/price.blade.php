90786d6f1e35113edb3d86c82b0e3fbaa8d255cb
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-03-24 11:52:02 +0100

    fix for price page

--- resources/views/pages/price.blade.php
+++ resources/views/pages/price.blade.php
@@ -29,14 +29,14 @@
         </x-button-group>
     </div>
 
-    <div x-data="@js(['nlg' => 1, 'eur' => round($currentPrice->price, 5), 'nlgPrice' => round($currentPrice->price, 5)])"
+    <div x-data="@js(['nlg' => 1, 'eur' => round($currentPrice?->price, 5), 'nlgPrice' => round($currentPrice?->price, 5)])"
         class="bg-white shadow overflow-hidden mt-4 sm:rounded-lg">
         <div class="px-4 py-5 sm:px-6">
             <h3 class="text-lg leading-6 font-medium text-gray-900">
                 Gulden converter
             </h3>
             <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                1 Gulden = €{{ round($currentPrice->price, 5) }}
+                1 Gulden = €{{ round($currentPrice?->price, 5) }}
             </p>
         </div>
 
