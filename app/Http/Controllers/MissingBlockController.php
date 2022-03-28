1dcb717ecee9d74b65df8cb4f403f7fdd9f62633
Bram van Berkel <BramVanBerkel@users.noreply.github.com>
2022-03-28 21:14:39 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/MissingBlockController.php
+++ app/Http/Controllers/MissingBlockController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Block;
 use App\Repositories\BlockRepository;
 
 class MissingBlockController extends Controller
 {
     public function __construct(
         private BlockRepository $blockRepository,
-    ) {}
-    
+    ) {
+    }
+
     public function __invoke(int $block)
     {
         $difference = ($block - $this->blockRepository->currentHeight()) * config('gulden.blocktime');
