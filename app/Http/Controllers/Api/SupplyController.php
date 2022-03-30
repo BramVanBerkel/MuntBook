8e1f6aa5089e1d668433929cd78cbb2bb6efc5fb
Bram van Berkel <BramVanBerkel@users.noreply.github.com>
2022-03-30 13:33:56 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Api/SupplyController.php
+++ app/Http/Controllers/Api/SupplyController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Repositories\BlockRepository;
 use App\Services\BlockService;
-use Illuminate\Http\Request;
 
 class SupplyController extends Controller
 {
     public function __construct(
         private BlockService $blockService,
         private BlockRepository $blockRepository,
-    ) {}
+    ) {
+    }
 
     /**
-     * Returns the total number of Guldens that will ever be mined
+     * Returns the total number of Guldens that will ever be mined.
+     *
      * @return int
      */
     public function totalSupply()
@@ -28,17 +29,18 @@ class SupplyController extends Controller
      *
      * To prevent long calculations, we start from a known block and supply.
      * At block 1.550.000 the supply was 561.870.520
-     * @param BlockService $blockService
+     *
+     * @param  BlockService  $blockService
      * @return int
      */
     public function circulatingSupply(BlockService $blockService)
     {
         $total = 561_870_520;
 
-        foreach(range(1_550_000, $this->blockRepository->currentHeight()) as $height) {
+        foreach (range(1_550_000, $this->blockRepository->currentHeight()) as $height) {
             $total += $this->blockService->getBlockSubsidy($height)->total();
         }
 
-        return (int)$total;
+        return (int) $total;
     }
 }
