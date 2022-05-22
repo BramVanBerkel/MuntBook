33ac9185c48ea2049bba32143ff8ce3754f9d140
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-05-22 17:22:02 +0200

    update deployer to v7

--- deploy.php
+++ deploy.php
@@ -1,70 +1,37 @@
 <?php
-
 namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Project name
-set('application', 'Guldenbook');
+// Config
+set('repository', 'https://bramvanberkel@github.com/BramVanBerkel/GuldenBook.git');
 
-// Project repository
-set('repository', 'git@github.com:BramVanBerkel/GuldenBook.git');
-
-// [Optional] Set gulden_url to download and install a new version of the binaries.
-set('gulden_url', null);
-
-// Shared files/dirs between deploys
 add('shared_files', [
-    '.env',
+    '.env'
 ]);
+
 add('shared_dirs', [
     'binaries',
 ]);
 
-// Writable dirs by web server
 add('writable_dirs', []);
 
 // Hosts
 host('guldenbook.com')
     ->set('deploy_path', '/var/www/guldenbook')
     ->set('testnet', false);
-
 host('testnet.guldenbook.com')
     ->set('deploy_path', '/var/www/guldenbook-testnet')
-    ->set('testnet', true);
-
-after('deploy:update_code', 'build');
+    ->set('tesnet', true);
 
 // Tasks
-task('build', [
-    'npm-ci',
-    'npm-build',
-    'composer-install',
-]);
-
-desc('Publish latest vendor assets');
-task('publish_assets', '{{bin/php}} {{release_path}}/artisan vendor:publish --tag=horizon-assets --force');
-
-after('deploy:vendors', 'publish_assets');
-
-desc('Run npm ci');
-task('npm-ci', function () {
-    run('cd {{release_path}} && npm ci');
-});
-
-desc('Run npm run production');
-task('npm-build', function () {
+task('build:frontend', function () {
     $testnet = (get('testnet')) ? 'true' : 'false';
-    run("cd {{release_path}} && MIX_TESTNET=$testnet npm run production");
+    cd('{{release_path}}');
+    run('npm ci');
+    run("MIX_TESTNET=$testnet npm run prod");
 });
 
-desc('Run composer install');
-task('composer-install', function () {
-    run('cd {{release_path}} && composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev');
-});
+after('deploy:update_code', 'build:frontend');
 
-// [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-before('deploy:symlink', 'artisan:migrate');
