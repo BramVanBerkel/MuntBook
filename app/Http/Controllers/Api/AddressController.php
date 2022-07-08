c2f6551486276525e8f7591e7f210ecea4870c49
Bram van Berkel <bramvanberkel.prive@gmail.com>
2022-07-08 12:11:16 +0200

    fix witness rewards calendar

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -6,6 +6,8 @@ use App\DataObjects\CalendarItem;
 use App\Http\Controllers\Controller;
 use App\Models\Vout;
 use Carbon\Carbon;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Support\Facades\DB;
 
 class AddressController extends Controller
@@ -41,7 +43,8 @@ class AddressController extends Controller
             ->join('transactions', 'vouts.transaction_id', '=', 'transactions.id')
             ->join('blocks', 'transactions.block_height', 'blocks.height')
             ->where('addresses.address', '=', $address)
-            ->where('vouts.type', '=', Vout::TYPE_WITNESS_REWARD)
+            ->whereIn('vouts.type', [Vout::TYPE_WITNESS_REWARD, Vout::TYPE_WITNESS])
+            ->whereDate('blocks.created_at', '>=', now()->subYear())
             ->groupBy('date')
             ->get()
             ->transform(fn (object $calendarItem): CalendarItem => new CalendarItem(
